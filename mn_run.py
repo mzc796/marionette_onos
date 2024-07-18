10b2d852728e2d2ff037ea5fb6a43946de0a45b2
Mingming Chen <55955582+mzc796@users.noreply.github.com>
2024-07-18 12:05:39 -0400

    Update mn_run.py

--- mn_run.py
+++ mn_run.py
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo mn --controller remote,ip=172.17.0.5 --controller remote,ip=172.17.0.6 --custom cus_topo_marionette.py --topo mytopo --switch ovs,protocols=OpenFlow13
+sudo mn --controller remote,ip=172.17.0.5 --controller remote,ip=172.17.0.6 --custom motivating_example_topo.py --topo mytopo --switch ovs,protocols=OpenFlow13
