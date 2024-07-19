8f474368493532018c105c1ac1acb67b69c8c202
Mingming Chen <55955582+mzc796@users.noreply.github.com>
2024-07-19 10:30:41 -0400

    Update sys_prep.sh

--- sys_prep.sh
+++ sys_prep.sh
@@ -4,10 +4,3 @@ sudo apt-get update
 sudo apt-get install vim mininet
 sudo apt install curl net-tools docker.io
 
-#Add $USER to the docker group
-sudo usermod -aG docker $1
-#Activate the changes to group
-newgrp docker
-
-# Change to $USER
-su - $1
